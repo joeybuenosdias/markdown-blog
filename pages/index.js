e3ef6638720fb21c3c7ba7f6689d5a7d75a5d45f
Joey Schrader <jschrader@overstock.com>
2020-12-06 11:49:22 -0800

    Map through posts on home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
-export default function Index() {
+import PropTypes from 'prop-types';
+import Link from 'next/link';
+
+export default function Index({ posts = [] }) {
 	return (
 		<div>
-			I am the Home page
-			I want to map through all the content here
+			{posts.map((item) => (
+				<Link key={item.title} href={`/posts/${item.slug}`}>
+					<div>
+						<h3>{item.title}</h3>
+						<p>{item.description}</p>
+					</div>
+				</Link>
+			))}
 		</div>
 	);
 }
 
-Index.getInitialProps = () => ({
-	title: 'Sookies Blog',
-	subtitle: 'I like fish',
-});
+Index.propTypes = {
+	posts: PropTypes.array.isRequired,
+};
+
+Index.getInitialProps = async () => {
+	const config = await import('../config.json');
+	const posts = config.default;
+
+	return { posts };
+};
